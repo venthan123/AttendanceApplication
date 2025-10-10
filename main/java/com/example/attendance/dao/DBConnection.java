e0243f134a7484cce852dbe45db18b9f1c39cdd5
BHAVENTHAN <bhaventhan08@gmail.com>
2025-10-10 15:19:20 +0530

    Update DBConnection.java

--- main/java/com/example/attendance/dao/DBConnection.java
+++ main/java/com/example/attendance/dao/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
     // Update these as needed for your local MySQL
     private static final String URL = "jdbc:mysql://localhost:3306/attendance_db?useSSL=false&serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASS = "6384";
+    private static final String PASS = "pass";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
